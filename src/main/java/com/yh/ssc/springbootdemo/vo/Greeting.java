1dc5145da65bfcd83020019fe8feb2b4da161e7f
yuanhaohy <mark.yuan@dianping.com>
2019-02-22 20:42:37 +0800

    hello world / package to jar

---
+++ src/main/java/com/yh/ssc/springbootdemo/vo/Greeting.java
@@ -0,0 +1,21 @@
+package com.yh.ssc.springbootdemo.vo;
+
+/**
+ * Create by yuanhaohy on 2019/2/22.
+ */
+public class Greeting {
+
+    private String content;
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
