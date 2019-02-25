a7e99f9611731948af86ec7c1b92d3af544372de
yuanhaohy <mark.yuan@dianping.com>
2019-02-25 20:09:12 +0800

    启动失败的错误信息

--- src/main/java/com/yh/ssc/springbootdemo/controller/HelloWorldController.java
+++ src/main/java/com/yh/ssc/springbootdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.yh.ssc.springbootdemo.controller;
 
+import com.yh.ssc.springbootdemo.service.DemoService;
 import com.yh.ssc.springbootdemo.vo.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private DemoService demoService;
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
---
+++ src/main/java/com/yh/ssc/springbootdemo/service/DemoService.java
@@ -0,0 +1,7 @@
+package com.yh.ssc.springbootdemo.service;
+
+/**
+ * Create by yuanhaohy on 2019/2/25.
+ */
+public class DemoService {
+}
